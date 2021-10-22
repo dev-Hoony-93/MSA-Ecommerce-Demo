1d8b0ab89decb9d3da758b618a8365aa6eef379c
hoony93 <hoon7566@gmail.com>
2021-10-22 18:04:08 +0900

    actuator test

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -34,7 +34,12 @@ public class UserController {
 
     @GetMapping("/health_check")
     public String status() {
-        return String.format("It's Working in User Service Port : {%s}",env.getProperty("local.server.port"));
+        return String.format("It's Working in User Service"
+                +" Port (local.server.port) = " +env.getProperty("local.server.port")
+                +" Port (server.port) = " +env.getProperty("server.port")
+                +" token secret = " +env.getProperty("token.secret")
+                +" token expiration time= " +env.getProperty("token.expiration_time")
+        );
     }
 
     @GetMapping("/welcome")
--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -32,6 +32,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
 //        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
         http.authorizeRequests().antMatchers("/**")
                         .hasIpAddress("127.0.0.1") // ip you want to pass // 통과시키고 싶은 id
                         .and()
