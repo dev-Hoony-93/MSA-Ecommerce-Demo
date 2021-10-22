7e46c5d1f848bd7855ec30c6a1787854741b418c
dev-Hoony-93 <hoon7566@gmail.com>
2021-10-23 01:54:22 +0900

    scg

--- scg/src/main/java/com/example/scg/ScgApplication.java
+++ scg/src/main/java/com/example/scg/ScgApplication.java
@@ -1,6 +1,8 @@
 package com.example.scg;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -10,4 +12,8 @@ public class ScgApplication {
         SpringApplication.run(ScgApplication.class, args);
     }
 
+
+    public HttpTraceRepository httpTraceRepository(){
+        return new InMemoryHttpTraceRepository();
+    }
 }
